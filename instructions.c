00c40ebc1ac3b5555ca98272693672fe4efebb3a
Jesus Quiaro Gamboa <jquiaro-@c1r1s5.42madrid.com>
2024-08-15 04:17:30 +0200

    1.2.7

--- instructions.c
+++ instructions.c
@@ -6,7 +6,7 @@
 /*   By: jquiaro- <jquiaro-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 17:46:12 by jquiaro-          #+#    #+#             */
-/*   Updated: 2024/08/15 02:30:01 by jquiaro-         ###   ########.fr       */
+/*   Updated: 2024/08/15 04:15:04 by jquiaro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	swap(t_stack *stack, char character, int value)
 {
 	t_node	*temp;
 
+	if (stack->head == NULL || stack->head->next == NULL)
+		return ;
 	temp = stack->head;
 	stack->head = temp->next;
 	temp->next = stack->head->next;
